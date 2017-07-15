8c33eb284d8f4b311cddceaa80febf7614010504
MyNameIsAngela <jinlin@jinlindeMacBook-Air.local>
2017-07-15 21:43:08 +0800

    Vue-response 获取数据 完成
    使用Vue-response 获取数据，此时控制台中可以看到访问的seller数据

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,24 @@
 <script>
 	import header from 'components/header/header.vue';
 
+	const ERR_OK = 0;
+
 	export default {
+		data(){
+			return {
+				seller: {}
+			};
+		},
+		created(){
+			this.$http.get('/api/seller').then((response) => {
+				response = response.body;
+				console.log(response);
+				if(response.errno === ERR_OK){
+					this.seller = response.data;
+					console.log(this.seller);
+				}
+			});
+		},
 		components:{
 			'v-header': header
 		}
