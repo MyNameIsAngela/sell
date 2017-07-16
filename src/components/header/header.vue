8f72e15b52a6283a9efbb0e3c1afdd26ac95eb64
MyNameIsAngela <jinlin@jinlindeMacBook-Air.local>
2017-07-16 21:43:36 +0800

    step 7-4 外部组件 4/6
    添加右下角的图标。“5个”

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,6 +17,10 @@
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
+			<div class="support-count" v-if="seller.supports">
+				<span class="count">{{seller.supports.length}}个</span>
+				<i class="icon-keyboard_arrow_right"></i>
+			</div>
 		</div>
 		<div class="bulletin-wrapper"></div>
 	</div>
@@ -39,8 +43,9 @@
 	@import"../../common/stylus/mixin";
 	.header
 		color: #fff
-		background: #000
+		background: #999
 		.content-wrapper
+			position: relative
 			padding: 24px 12px 18px 24px //上右下左
 			font-size: 0
 			.avatar
@@ -92,5 +97,22 @@
 							bg-image('special_1')
 					.text
 						line-hight: 12px
-						font-size: 12px		
+						font-size: 10px		
+			.support-count
+				position: absolute			
+				right: 12px
+				bottom: 14px
+				padding: 0 8px
+				height: 24px
+				line-height: 24px
+				border-radius: 14px
+				background: rgba(0,0,0,0.2)
+				text-align: center
+				.count
+					vertical-align: top
+					font-size: 10px
+				.icon-keyboard_arrow_right
+					margin-left: 2px
+					line-height: 24px
+					font-size: 10px
 </style>
