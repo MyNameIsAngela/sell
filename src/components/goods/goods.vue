49237f203e2e2d9854a7fae62374c8400fff2ccb
MyNameIsAngela <jinlin@jinlindeMacBook-Air.local>
2017-07-19 14:59:17 +0800

    第7章 项目实战-goods 商品列表页开发--布局编写
    布局编写，左右两栏布局
    取得goods数据，在console中显示

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,47 @@
 <template>
-	<div>i am goods</div>
+	<div class="goods">
+		<div class="menu-wrapper"></div>
+		<div class="foods-wrapper"></div>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
-	export default {};
+	const ERR_OK = 0;
+	export default {
+		props:{
+			seller:{
+				type: Object
+			}
+		},
+		data(){
+			return {
+				goods: []
+			};
+		},
+		created(){
+			this.$http.get('/api/goods').then((resopnse)=> {
+				resopnse = resopnse.body;
+				if(resopnse.errno === ERR_OK) {
+					this.goods = resopnse.data;
+					console.log(this.goods);
+				}
+			});
+		}
+	};
 </script>
 
 <style lang="stylus" res="stylesheet/stylus">
-	
+	.goods
+		display: flex
+		position: absolute
+		top: 174px
+		bottom: 46px
+		width: 100%
+		overflow: hidden
+		.menu-wrapper
+			flex: 0 0 80px
+			width: 80px
+			backgroung: #f3f5f7
+		.foods-wrapper
+			flex: 1
 </style>
